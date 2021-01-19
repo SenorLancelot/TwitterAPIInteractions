78b0ed0e9e943d82fd269ebf7e528d46069c9542
SenorLancelot <amarshendkar.as@gmail.com>
2021-01-19 21:35:19 +0530

    Initial Upload

---
+++ hidden.py
@@ -0,0 +1,5 @@
+def oauth():
+    return {"consumer_key": "F8QaTKVXWZTrUDU5bej5J7fxh",
+            "consumer_secret": "Ur8M6adFU9aCSsNwYwAM10wtgwyPhi2JZKqAm3Ic9E92CnSqhX",
+            "token_key": "2788443832-iLpCwEszbw6ZDMgCR4cMvZ4lc8YQ2ECXbwZktxC",
+            "token_secret": "uHykRpKuHzedxbAbbqyPBL6p44WVIXTIwnvC8sWDo2hk9"}
